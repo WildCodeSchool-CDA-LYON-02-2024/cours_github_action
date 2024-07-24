df73f61f2d8262a48986c658cad16aa6c41e507d
William T. <william.tahar@gmail.com>
2024-07-24 17:26:10 +0200

    break sum test

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -3,6 +3,6 @@ import { sum } from './sum';
 
 describe('SUM module', () => {
     test('adds 1 + 2 to equal 3', () => {
-        expect(sum(1, 2)).toBe(3)
+        expect(sum(2, 2)).toBe(3)
     });
 })
